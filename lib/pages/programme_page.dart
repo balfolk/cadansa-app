f68f518495f60235b28cb9657652a4ac5940bfa2
lbel <lennaert@lbel.nl>
2019-09-15 11:55:54 +0200

    Support having only start time, plus some cleanup

--- lib/pages/programme_page.dart
+++ lib/pages/programme_page.dart
@@ -23,16 +23,17 @@ class _ProgrammePageState extends State<ProgrammePage> {
   @override
   Widget build(final BuildContext context) {
     return DefaultTabController(
-        initialIndex: _initialIndex,
-        length: widget._programme.days.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(widget._title),
-            bottom: TabBar(tabs: tabs),
-          ),
-          body: TabBarView(children: tabChildren),
-          bottomNavigationBar: widget._bottomBarGenerator(),
-        ));
+      initialIndex: _initialIndex,
+      length: widget._programme.days.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget._title),
+          bottom: TabBar(tabs: tabs),
+        ),
+        body: TabBarView(children: tabChildren),
+        bottomNavigationBar: widget._bottomBarGenerator(),
+      ),
+    );
   }
 
   int get _initialIndex {
@@ -67,8 +68,14 @@ class _ProgrammePageState extends State<ProgrammePage> {
           final ProgrammeItem item = day.items[index];
 
           Widget subtitle;
-          if (item.startTime != null && item.endTime != null) {
-            subtitle = Text('${item.startTime.format(context)} – ${item.endTime.format(context)}');
+          if (item.startTime != null || item.endTime != null) {
+            String text;
+            if (item.startTime != null && item.endTime != null) {
+              text = '${item.startTime.format(context)} – ${item.endTime.format(context)}';
+            } else {
+              text = '${(item.startTime ?? item.endTime).format(context)}';
+            }
+            subtitle = Text(text);
           }
 
           final Widget header = ListTile(
@@ -133,19 +140,26 @@ class _ProgrammePageState extends State<ProgrammePage> {
         days: item.startTime.hour < HOUR_NIGHT_CUTOFF ? 1 : 0,
         hours: item.startTime.hour,
         minutes: item.startTime.minute));
-    final endMoment = startDay.add(Duration(
+    final endMoment = item.endTime != null ? startDay.add(Duration(
         days: item.endTime.hour < HOUR_NIGHT_CUTOFF ? 1 : 0,
         hours: item.endTime.hour,
-        minutes: item.endTime.minute));
+        minutes: item.endTime.minute)) : null;
     final now = DateTime.now();
     if (now.isBefore(startMoment)) return _PlayingStatus.before;
-    if (now.isAfter(endMoment)) return _PlayingStatus.after;
-    return _PlayingStatus.during;
+    if (endMoment != null) {
+      if (now.isAfter(endMoment)) return _PlayingStatus.after;
+      return _PlayingStatus.during;
+    } else {
+      return _PlayingStatus.after;
+    }
   }
 
   Widget _getIcon(final ProgrammeDay day, final ProgrammeItem item) {
     final IconData iconData = MdiIcons.fromString(item.kind.icon);
-    if (iconData == null) return null;
+    if (iconData?.codePoint == null) {
+      print('Invalid icon ${item.kind.icon}');
+      return null;
+    }
 
     final status = _getPlayingStatus(day, item);
     final color = status == _PlayingStatus.after
