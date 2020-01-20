4c102024af4af3262c771216fa5f19dd76708810
lbel <lennaert@lbel.nl>
2020-01-20 10:53:13 +0100

    Allow adding clickable areas to the map

--- lib/data/map.dart
+++ lib/data/map.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cadansa_app/data/parse_utils.dart';
 
 class MapData {
@@ -18,17 +20,18 @@ class Floor {
   final LText _url;
   final int _version;
   final double _initialScale, _minScale, _maxScale;
+  final List<FloorArea> _areas;
 
-  Floor._(this._title, this._url, this._version, this._initialScale, this._minScale, this._maxScale);
+  Floor._(this._title, this._url, this._version, this._initialScale, this._minScale, this._maxScale, this._areas);
 
-  Floor.parse(final dynamic json)
-      : this._(
+  Floor.parse(final dynamic json) : this._(
       LText(json['title']),
       LText(json['path']),
       json['version'] as int,
       json['initialScale'] as double,
       json['minScale'] as double,
-      json['maxScale'] as double);
+      json['maxScale'] as double,
+      List.unmodifiable((json['areas'] ?? []).map((area) => FloorArea.parse(area))));
 
   LText get title => _title;
 
@@ -41,4 +44,26 @@ class Floor {
   double get minScale => _minScale;
 
   double get maxScale => _maxScale;
+
+  List<FloorArea> get areas => _areas;
+}
+
+class FloorArea {
+  final Path _path;
+  final String _action;
+  final Offset _center;
+
+  FloorArea._(this._path, this._action, this._center);
+
+  FloorArea.parse(final dynamic json) : this._(
+    Path()..addPolygon(json['path'].map((point) => Offset(point[0].toDouble(), point[1].toDouble())).cast<Offset>().toList(), true),
+    json['action'],
+    Offset(json['center'][0].toDouble(), json['center'][1].toDouble()),
+  );
+
+  Path get path => Path.from(_path);
+
+  String get action => _action;
+
+  Offset get center => _center;
 }
