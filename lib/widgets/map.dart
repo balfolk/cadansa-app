a8944ec544a225c0dba63f1a8d080818189c41cc
lbel <lennaert@lbel.nl>
2020-01-20 08:16:22 +0100

    Add map load failure text

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,10 +1,17 @@
 import 'package:cadansa_app/data/map.dart';
+import 'package:cadansa_app/data/parse_utils.dart';
 import 'package:flutter/material.dart';
 
 import 'package:photo_view/photo_view.dart';
 
 class MapWidget extends StatelessWidget {
   final Floor _data;
+
+  static final LText _MAP_LOAD_FAIL_TEXT = LText(const {
+    'en': "Could not load the festival map. Please make sure you're connected to the Internet.",
+    'nl': 'Het is niet gelukt om de plattegrond te downloaden. Controleer of je internetverbinding aanstaat.',
+    'fr': 'Échec du téléchargement du plan. Assurez-vous que votre connexion Internet fonctionne.'
+  });
 
   MapWidget(this._data);
 
@@ -19,7 +26,21 @@ class MapWidget extends StatelessWidget {
         minScale: PhotoViewComputedScale.contained,
         maxScale: _data.maxScale ?? 1.0,
         backgroundDecoration: BoxDecoration(color: Colors.white),
+        filterQuality: FilterQuality.medium,
+        loadFailedChild: _buildLoadFailedChild(),
       ),
     );
   }
+  
+  Widget _buildLoadFailedChild() => Builder(builder: (context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Text(
+        _MAP_LOAD_FAIL_TEXT.get(Localizations.localeOf(context)),
+        textAlign: TextAlign.center,
+      ),
+    );
+  });
+  
 }
