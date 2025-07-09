af21c2af250526927999d35ab7fb890173841feb
Lennaert Bel <lennaert@minviro.com>
2025-07-09 22:28:54 +0200

    Update iOS build system

--- build_all.sh
+++ build_all.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Building IPA (iOS/iPadOS)..."
-flutter build ipa --no-tree-shake-icons --obfuscate --split-debug-info=./ios/
+if ! type flutter &>/dev/null -a type fvm &>/dev/null; then
+  echo 'Flutter not found, but fvm is. Using fvm flutter.'
+  fvm use
+  FLUTTER_CMD='fvm flutter'
+else
+  FLUTTER_CMD='flutter'
+fi
+
+echo 'Building IPA (iOS/iPadOS)...'
+${FLUTTER_CMD} build ipa --no-tree-shake-icons --obfuscate --split-debug-info=./ios/
 echo
 
-echo "Building AppBundle (Android)..."
-flutter build appbundle --no-tree-shake-icons --obfuscate --split-debug-info=./android/
-
-rm -f android/symbols.zip
-(cd build/app/intermediates/merged_native_libs/release/out/lib && zip --recurse-paths ../../../../../../../android/symbols.zip ./* >/dev/null)
+echo 'Building AppBundle (Android)...'
+${FLUTTER_CMD} build appbundle --no-tree-shake-icons --obfuscate --split-debug-info=./android/
