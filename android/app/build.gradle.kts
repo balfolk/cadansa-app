ffdc14e91049a2e100381dca34409a211fed4bcc
Lennaert Bel <lennaert@minviro.com>
2025-10-08 22:46:01 +0200

    Update Flutter, dependencies, and build config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
